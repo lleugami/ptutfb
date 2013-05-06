bfd7e7072052d5d6541f133d1ff13d242a641f46
root <root@debianLP2013.localdomain>
2013-05-06 19:51:14 +0200

    admin controller

---
+++ src/Metinet/Bundle/FacebookBundle/Controller/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Metinet\Bundle\FacebookBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+
+}
