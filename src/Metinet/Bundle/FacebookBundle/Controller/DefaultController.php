0ec5dc360955525b17e6ef79306079ee09ad23c0
root <root@debianLP2013.localdomain>
2013-05-13 22:30:19 +0200

    version 3

--- src/Metinet/Bundle/FacebookBundle/Controller/DefaultController.php
+++ src/Metinet/Bundle/FacebookBundle/Controller/DefaultController.php
@@ -19,8 +19,10 @@ class DefaultController extends Controller
         //$friends = $this->container->get('metinet.manager.fbuser')->getUserFriends("me");
         $repository = $this->getDoctrine()->getRepository('MetinetFacebookBundle:User');
         $classement = $repository->getClassement($user['id']);
-        
-        return array('classement' => $classement);
+
+        $themes = $this->showThemeAction();
+
+        return array('classement' => $classement,'themes' => $themes);
     }
 
     /**
@@ -32,5 +34,50 @@ class DefaultController extends Controller
         return array();
     }
 
+
+    public function showThemeAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('MetinetFacebookBundle:Theme')->findAll();
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Question entity.');
+        }
+
+
+                
+        foreach ($entity as $key => $value) {
+            if($value->getQuizzes() != "")
+            {
+                $tab[] = $value->getTitle();
+            }
+           
+        }
+
+        print_r($tab);
+        exit();
+
+
+
+        return $entity;
+    }
+
+
+    /**
+     * @Route("/{id}", name="list_quizz")
+     * @Template()
+     */
+    public function listAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('MetinetFacebookBundle:Quizz')->findBy(array('theme' => $id));
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Quizz entity.');
+        }
+        return  array('quizz' => $entity);
+    }
 
 }
